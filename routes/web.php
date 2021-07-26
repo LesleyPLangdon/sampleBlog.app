466b112abbc6eb2a49c279fbf9bbef905c16afe2
LesleyPLangdon <61936741+LesleyPLangdon@users.noreply.github.com>
2021-07-26 15:33:19 -0400

    Complete through lesson 35, section 5

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,27 +20,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-          'posts' => Post::latest()//->with('category', 'author')->get()
+        'posts' => Post::latest()->get(),//->with('category', 'author')->get()
+        'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 //variable($) name must match wildcard({}) name
-Route::get('posts/{post:slug}', function(Post $post) {
+Route::get('posts/{post:slug}', function (Post $post) {
 
     return view('post', [
         'post' => $post
+
     ]);
 });
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts//->load(['category', 'author'])
+        'posts' => $category->posts,//->load(['category', 'author'])
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
-});
+})->name('category');
 
-    Route::get('authors/{author:username}', function (User $author) {
-        return view('posts', [
-            'posts' => $author->posts//->load(['category', 'author'])
-        ]);
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts,//->load(['category', 'author'])
+        'categories' => Category::all()
+    ]);
 });
 
